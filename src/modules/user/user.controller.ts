b4f640c8e28e54e1344046a7db67dad08cafa68c
Mehedi <mehedihasan712277@gmail.com>
2026-07-02 19:55:04 +0600

    feat: user register function created

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { userService } from "./user.service";
+
+const registerUser = async (req: Request, res: Response) => {
+    try {
+        const payload = req.body;
+
+        const user = await userService.createUserIntoDB(payload);
+
+        res.status(httpStatus.CREATED).json({
+            success: true,
+            statusCode: httpStatus.CREATED,
+            message: "user registered successfully",
+            data: {
+                user,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+            message: "Failed to register user",
+            error: (error as Error).message,
+        });
+    }
+};
+
+export const userController = { registerUser };
